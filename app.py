06579b4745693ce4fd0615c9942bbadadeb965d3
asbag4 <162579350+asbag4@users.noreply.github.com>
2026-05-17 22:24:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
 import streamlit as st
 
-st.title("Counter App 🔢")
-
-# Create counter if it doesn't exist
+st.title("Counter App")
 if "count" not in st.session_state:
     st.session_state.count = 0
-
-# Show count
 st.header(st.session_state.count)
 
-# Buttons
 col1, col2, col3 = st.columns(3)
-
 with col1:
-    if st.button("➕ Increase"):
+    if st.button("➕ increase"):
         st.session_state.count += 1
-
 with col2:
     if st.button("➖ Decrease"):
         st.session_state.count -= 1
-
 with col3:
     if st.button("🔄 Reset"):
         st.session_state.count = 0
